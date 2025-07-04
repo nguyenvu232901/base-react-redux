275e9e3c89ea1a4917548cbeb23fd96962b1bd00
nguyenvu232901 <97593493+nguyenvu232901@users.noreply.github.com>
2025-07-04 23:46:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
